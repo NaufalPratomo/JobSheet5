c30f8564f25372acc2f10f36dc8e78e3477a202e
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-09-25 12:37:59 +0700

    pemilihan percobaan modifikasi

---
+++ PemilihanPercobaan119modifikasi.java
@@ -0,0 +1,8 @@
+public class PemilihanPercobaan119modifikasi {
+    public static void main(String[] args) {
+
+        int angka = 9;
+        String hasil = (angka % 2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
+        System.out.println(angka + " adalah " + hasil);
+    }
+}
