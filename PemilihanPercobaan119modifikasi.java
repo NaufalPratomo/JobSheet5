c29807825ff6bf6b2ac1a5ed346665469f688066
NaufalPratomo <140324988+NaufalPratomo@users.noreply.github.com>
2023-09-25 13:11:05 +0700

    PemilihanPercobaan119modifikasi.java

--- PemilihanPercobaan119modifikasi.java
+++ PemilihanPercobaan119modifikasi.java
@@ -1,7 +1,12 @@
+import java.util.Scanner;
+
 public class PemilihanPercobaan119modifikasi {
     public static void main(String[] args) {
 
-        int angka = 9;
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukan Angka: ");
+
+        int angka = input.nextInt();
         String hasil = (angka % 2 == 0) ? "Bilangan Genap" : "Bilangan Ganjil";
         System.out.println("angka " + angka + " " + hasil);
     }
