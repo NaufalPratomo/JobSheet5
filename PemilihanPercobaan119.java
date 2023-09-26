46a17cd8b5c4848ab219d28bead727868e8ddd25
Naufal Pratomo <naufalpratomo8@gmail.com>
2023-09-26 20:10:58 +0700

    .

--- PemilihanPercobaan119.java
+++ PemilihanPercobaan119.java
@@ -4,7 +4,7 @@ public class PemilihanPercobaan119 {
     public static void main(String[] args) {
     Scanner input19 = new Scanner(System.in);
 
-    System.out.println("Masukan angka: ");
+    System.out.print("Masukan angka: ");
     int angka = input19.nextInt();
 
     if (angka % 2 == 0) {
